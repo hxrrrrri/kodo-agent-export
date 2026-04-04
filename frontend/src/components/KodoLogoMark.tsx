c6b79ce195a7cec4a86e31f29ff822b0e496c4bd
hxrrrrri <harisankars.mbcet@gmail.com>
2026-04-04 12:35:35 +0530

    feat: update KodoLogoMark SVG viewBox and adjust logo colors in globals.css

--- frontend/src/components/KodoLogoMark.tsx
+++ frontend/src/components/KodoLogoMark.tsx
@@ -16,7 +16,7 @@ export function KodoLogoMark({
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
+      viewBox="-6 -4 112 108"
       width={size}
       height={size}
       className={className || undefined}
@@ -26,10 +26,12 @@ export function KodoLogoMark({
       aria-label={decorative ? undefined : title}
       style={{ display: 'block' }}
     >
-      <path d="M 28 15 C 18 15, 18 45, 8 50 C 18 55, 18 85, 28 85 L 28 73 C 22 73, 22 58, 16 50 C 22 42, 22 27, 28 27 Z" />
-      <rect x="34" y="15" width="8" height="70" />
-      <path d="M 42 55 C 52 45, 62 30, 68 25 L 56 25 L 76 12 L 80 32 L 68 25 C 62 32, 52 48, 42 62 Z" />
-      <path d="M 48 57 C 58 64, 65 75, 75 85 L 85 85 C 75 75, 65 60, 50 51 Z" />
+      <g transform="translate(50 50) scale(1.11 1.07) translate(-50 -50)">
+        <path d="M 28 15 C 18 15, 18 45, 8 50 C 18 55, 18 85, 28 85 L 28 73 C 22 73, 22 58, 16 50 C 22 42, 22 27, 28 27 Z" />
+        <rect x="34" y="15" width="8" height="70" />
+        <path d="M 42 55 C 52 45, 62 30, 68 25 L 56 25 L 76 12 L 80 32 L 68 25 C 62 32, 52 48, 42 62 Z" />
+        <path d="M 48 57 C 58 64, 65 75, 75 85 L 85 85 C 75 75, 65 60, 50 51 Z" />
+      </g>
     </svg>
   )
 }
